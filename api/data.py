afe2ee05fafef23670a5d935d830e3ac025e4a97
ASHALIVE <bdiptvworld@gmail.com>
2026-09-03 17:14:26 +0600

    Change Redis key from 'all_movies_db' to 'data_json_db'

--- api/data.py
+++ api/data.py
@@ -6,11 +6,12 @@ redis = Redis.from_env()
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        all_movies_raw = redis.get("all_movies_db")
+        all_movies_raw = redis.get("data_json_db")
         all_movies_dict = json.loads(all_movies_raw) if all_movies_raw else {}
         
         all_items = list(all_movies_dict.values())
 
+        # আপনার চাহিদামতো JSON ফরম্যাট
         output = {
             "hero": all_items[:5] if len(all_items) >= 5 else all_items,
             "categories": [
